6e9b1795bcbf8f1c80f11d7b041f2518ffd98703
Diana Chukhrai <dianadenwik@gmail.com>
2026-02-23 14:09:10 +0100

    feat: implement courseComand

--- src/index.js
+++ src/index.js
@@ -4,21 +4,22 @@ import { parseCommand } from './command-parser.js';
 
 import { handleTraineeCommand } from './traineeCommands.js';
 
-
+import { handleCourseCommand } from './courseCommands.js';
 
 const prompt = promptSync();
 
 const input = prompt('> ');
 
-const parsedCmd = parseCommand(input); // 
-// This is the entry point of your application.
-// Ask user for input, parse the command, and call the appropriate function from courseCommands.js
-//  or traineeCommands.js based on the command.
+const parsedCmd = parseCommand(input); //
+
 
 console.log(parsedCmd);
 
 if (parsedCmd.command === 'TRAINEE') {
-   handleTraineeCommand(parsedCmd.subcommand, parsedCmd.args)
-
+  handleTraineeCommand(parsedCmd.subcommand, parsedCmd.args);
+} else if (parsedCmd.command === 'COURSE') {
+  handleCourseCommand(parsedCmd.subcommand, parsedCmd.args);
 }
-//else if
+else {
+    console.log('Enter the correct command')
+}
