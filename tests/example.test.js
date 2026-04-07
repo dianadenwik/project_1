287fc3807117b02134dab36994c05e73a0ca866a
Diana Chukhrai <dianadenwik@gmail.com>
2026-04-07 17:19:29 +0200

    Fix test

--- tests/example.test.js
+++ tests/example.test.js
@@ -2,7 +2,7 @@ import { describe, expect, test } from 'vitest';
 
 describe('Example Test', () => {
   test('adds 1 + 2 to equal 3', () => {
-    expect(1 + 2).toBe(4);
+    expect(1 + 2).toBe(3);
   });
 
   // Add your tests here. You can create multiple test files in the `tests` directory.
